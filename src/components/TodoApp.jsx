e814fc4f8a0086bab411a94a8fcf1df3bcba70a2
bocong <bocongyin@gmail.com>
2016-04-27 22:28:23 -0500

    Initial commit

---
+++ src/components/TodoApp.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TodoList from './TodoList';
+
+export default class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <section className="todoapp">
+          <TodoList todos={this.props.todos} />
+        </section>
+      </div>
+    );
+  }
+};
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import TodoItem from './TodoItem';
+
+export default class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+
+  render() {
+    return (
+      <section className="main">
+        <ul className="todo-list">
+          {this.props.todos.map(item =>
+            <TodoItem key={item.get('text')}
+                      text={item.get('text')} />
+          )}
+        </ul>
+      </section>
+    );
+  }
+};
